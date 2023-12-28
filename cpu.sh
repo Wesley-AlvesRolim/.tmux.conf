7ec281de16146b6e688d018b7909633032636f31
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-12-28 11:52:04 -0300

    style: format

--- cpu.sh
+++ cpu.sh
@@ -5,15 +5,15 @@ CPU_PERCENTAGE="";
 
 main()
 {
-	while [[ true ]]; do
-		sleep $INTERVAL;
-		CPU_PERCENTAGE=$(top -bn1 |
-			grep "CPU(s)" |
-			sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |
-			awk '{print $1"%"}'
-		);
-		echo $CPU_PERCENTAGE;
-	done;
+  while [[ true ]]; do
+    sleep $INTERVAL;
+    CPU_PERCENTAGE=$(top -bn1 |
+      grep "CPU(s)" |
+      sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |
+      awk '{print $1"%"}'
+    );
+    echo $CPU_PERCENTAGE;
+  done;
 }
 
 main
--- ram.sh
+++ ram.sh
@@ -5,14 +5,14 @@ MEM_USAGE="";
 
 main()
 {
-	while [[ true ]]; do
-		sleep $INTERVAL;
-		MEM_USAGE=$(top -bn1 |
-			grep "MB mem" |
-			awk '{printf "%.2f GB\n", ($8 / 1000)}'
-		);
-		echo $MEM_USAGE;
-	done;
+  while [[ true ]]; do
+    sleep $INTERVAL;
+    MEM_USAGE=$(top -bn1 |
+      grep "MB mem" |
+      awk '{printf "%.2f GB\n", ($8 / 1000)}'
+    );
+    echo $MEM_USAGE;
+  done;
 }
 
 main
